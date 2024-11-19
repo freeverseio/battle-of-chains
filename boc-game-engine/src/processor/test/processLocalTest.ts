f054e89f252d0cddeb72b31bbfd229698014a6ce
Toni <>
2024-11-19 10:28:25 +0100

    updating all tests

--- boc-game-engine/src/processor/test/processLocalTest.ts
+++ boc-game-engine/src/processor/test/processLocalTest.ts
@@ -23,7 +23,7 @@ async function test(debugData: DebugData) {
 async function main() {
     const debugData = {
         "gameStartTime": 1729168020,
-        "deadline": 1731402544,
+        "deadline": 1732008362,
         "useHardcodedEvents": false,
         "eventsFile": './src/processor/test/events02.json',
         "storageFile": './src/processor/test/storage02.json',
