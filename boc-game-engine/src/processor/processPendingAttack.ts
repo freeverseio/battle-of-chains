dcd72b39cd6b6cf91925ef6ee67f95199eb5bd1a
Toni <>
2024-12-03 14:56:42 +0100

    renaming variable

--- boc-game-engine/src/processor/processPendingAttack.ts
+++ boc-game-engine/src/processor/processPendingAttack.ts
@@ -49,47 +49,47 @@ function processAttackArrival(attack: PendingAttack, storage: Storage) {
     const attackerAttack = availableAttackerAssets.reduce((sum, asset) => sum + asset.attack, 0);
     const attackerDefense = availableAttackerAssets.reduce((sum, asset) => sum + asset.defense, 0);
  
-    // Attacked:
+    // Target:
     const targetUser = findUser(attack.targetAddress, storage.users);
     const isTargetUserInHomechain = targetUser?.homechain === attack.targetChain;
     const defenseBoostHomechain = isTargetUserInHomechain ? DEFENSE_BOOST_HOMECHAIN : 1;
 
-    const attackedAssets = getFreeInventoryInChain(attack.targetAddress, attack.targetChain, storage.assets);
-    evolveAssetsStats(attack.toBeExectutedAt, attackedAssets, storage);
-    const availableAttackedAssets = getAlive(attackedAssets);
-    const attackedAttack = availableAttackedAssets.reduce((sum, asset) => sum + asset.attack, 0);
-    const attackedDefense = availableAttackedAssets.reduce((sum, asset) => sum + asset.defense, 0) * defenseBoostHomechain;
+    const targetAssets = getFreeInventoryInChain(attack.targetAddress, attack.targetChain, storage.assets);
+    evolveAssetsStats(attack.toBeExectutedAt, targetAssets, storage);
+    const availableTargetAssets = getAlive(targetAssets);
+    const targetAttack = availableTargetAssets.reduce((sum, asset) => sum + asset.attack, 0);
+    const targetDefense = availableTargetAssets.reduce((sum, asset) => sum + asset.defense, 0) * defenseBoostHomechain;
 
     evolveTreasuryByAddress(attack.attacker, attack.toBeExectutedAt, storage);
     evolveTreasuryByAddress(attack.targetAddress, attack.toBeExectutedAt, storage);
 
     const attackerTreasury = getUserTreasury(attack.attacker, storage.users);
-    const attackedTreasury = getUserTreasury(attack.targetAddress, storage.users);
+    const targetTreasury = getUserTreasury(attack.targetAddress, storage.users);
 
-    const seed = murmurhash.v3(`${attack.blockHash}${storage.processedPendingIdx}${attackerTreasury}${attackedTreasury}`);
+    const seed = murmurhash.v3(`${attack.blockHash}${storage.processedPendingIdx}${attackerTreasury}${targetTreasury}`);
     const rnds = computeRandoms(4, seed);
     const maxRndValue = 2**32 - 1;
     // one asset of level N+1 is typically STATS_FACTOR_TO_NEXT_LEVEL stronger than one asset at level N
     // We want that a 1:1 of N+1 against N makes 50% of damage
-    const damageHPPercentOnTarget =  Math.min(100, Math.round(50 * (rnds[0]/maxRndValue) * (attackerAttack/attackedDefense)));
-    const damageHPPercentOnAttacker = Math.min(100, Math.round(50 * (rnds[1]/maxRndValue) * (attackedAttack/attackerDefense)));
+    const damageHPPercentOnTarget = Math.min(100, Math.round(50 * (rnds[0]/maxRndValue) * (attackerAttack/targetDefense)));
+    const damageHPPercentOnAttacker = Math.min(100, Math.round(50 * (rnds[1]/maxRndValue) * (targetAttack/attackerDefense)));
 
     const increaseHPPercentForAttacker = Math.min(10, Math.round(damageHPPercentOnTarget / 5));
-    const increaseHPPercentForAttacked = Math.min(10, Math.round(damageHPPercentOnAttacker / 5));
+    const increaseHPPercentForTarget = Math.min(10, Math.round(damageHPPercentOnAttacker / 5));
 
     let attackerCasulaties = 0;
-    let attackedCasulaties = 0;
+    let targetCasulaties = 0;
 
 
-    const averageAttackedDefense = attackedDefense / attackedAssets.length;
-    for (const asset of attackedAssets) {
-        decreaseAssetHealthByPercent(asset, adaptPercetangeToAverage(damageHPPercentOnTarget, asset.defense, averageAttackedDefense));
+    const averageTargetDefense = targetDefense / targetAssets.length;
+    for (const asset of targetAssets) {
+        decreaseAssetHealthByPercent(asset, adaptPercetangeToAverage(damageHPPercentOnTarget, asset.defense, averageTargetDefense));
         if (asset.health === 0) {
-            attackedCasulaties++;
+            targetCasulaties++;
             reportDeath(asset, `Attack by ${attack.attacker}.`, attack.toBeExectutedAt, storage);
         } else {
             if (!isFactory(asset.type)){
-                increaseAssetXPByPercent(asset, increaseHPPercentForAttacked * asset.potential / AVERAGE_POTENTIAL);
+                increaseAssetXPByPercent(asset, increaseHPPercentForTarget * asset.potential / AVERAGE_POTENTIAL);
             }
         }
     }
@@ -120,15 +120,15 @@ function processAttackArrival(attack: PendingAttack, storage: Storage) {
     if (increaseHPPercentForAttacker > 0) attackerComment += `. Your troops gained ${increaseHPPercentForAttacker} percentual XP points`;
     if (attackerCasulaties > 0) attackerComment += `. You lost ${attackerCasulaties} assets in the fight`;
 
-    let attackedComment = `You were attacked on ${chainName(attack.targetChain, storage.chains)} by ${attack.attacker}; they stole ${subtractedAmount} coins, and attacked you with ${damageHPPercentOnTarget}% success; you backfired and harmed them with ${damageHPPercentOnAttacker}% success`;
-    if (increaseHPPercentForAttacked > 0) attackedComment += `. Your troops gained ${increaseHPPercentForAttacked} percentual XP points`;
-    if (attackedCasulaties > 0) attackedComment += `. You lost ${attackedCasulaties} assets in the fight`;
+    let targetComment = `You were attacked on ${chainName(attack.targetChain, storage.chains)} by ${attack.attacker}; they stole ${subtractedAmount} coins, and attacked you with ${damageHPPercentOnTarget}% success; you backfired and harmed them with ${damageHPPercentOnAttacker}% success`;
+    if (increaseHPPercentForTarget > 0) targetComment += `. Your troops gained ${increaseHPPercentForTarget} percentual XP points`;
+    if (targetCasulaties > 0) targetComment += `. You lost ${targetCasulaties} assets in the fight`;
 
     log2user(attack.attacker, attackerComment, attack.toBeExectutedAt, storage.logs);
-    log2user(attack.targetAddress, attackedComment, attack.toBeExectutedAt, storage.logs);
+    log2user(attack.targetAddress, targetComment, attack.toBeExectutedAt, storage.logs);
 
-    // If the attacked user was in a homechain, and no assets were left to defend it, damage the homechain.
-    if (isTargetUserInHomechain && (attackedCasulaties >= availableAttackedAssets.length)) {
+    // If the target user was in a homechain, and no assets were left to defend it, damage the homechain.
+    if (isTargetUserInHomechain && (targetCasulaties >= availableTargetAssets.length)) {
         killHomechain(targetUser, attack.toBeExectutedAt, storage);
     }
 }
