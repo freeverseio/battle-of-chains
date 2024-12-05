75c553d4753bdd9edb9582a25d7fcab59390834d
Daniel Fuyà Espinar <danifuya@gmail.com>
2024-12-05 10:40:59 +0100

    map tooltip

--- boc-ui/constants/tooltips.tsx
+++ boc-ui/constants/tooltips.tsx
@@ -8,5 +8,7 @@ export const tooltips = {
   players: "View other players in the game, their assets, and engage in combat. Select players to view their assets and initiate attacks",
   universeSummary: "View a chronological log of all activities happening across the universe, including attacks, upgrades, and chain actions",
   chainDailyDecision: "Vote on daily actions for your chain, including improvements, defense strategies, and coordinated attacks",
-  chainActionProposals: "View all current proposals across different chains and their vote counts. Each chain can execute one action per day based on votes"
+  chainActionProposals: "View all current proposals across different chains and their vote counts. Each chain can execute one action per day based on votes",
+  map: "Map",
+
 } 
