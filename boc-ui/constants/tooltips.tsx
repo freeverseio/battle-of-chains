9707be23ad860e1a596d180a312369d7d25088c0
Toni <>
2024-12-05 12:15:52 +0100

    fixing typos

--- boc-ui/constants/tooltips.tsx
+++ boc-ui/constants/tooltips.tsx
@@ -1,14 +1,14 @@
 // src/constants/tooltips.ts
 // src/constants/tooltips.ts
 export const tooltips = {
-  userInformation: "Homechain: the chain you support, which allows you to vote for its Chain Daily Actions. Score: your overall score, earned via different actions in the game",
-  userActivity: "All events related to you: your mints, attacks and upgrades, those that plan attaking you, etc.",
-  homeBase: "Your Home Base generates treasury resources constantly. Upgrade: attempts to level up your home base, to increase its production rate; the upgrade will not be applied unless you have enough balance in your treasury. XP: your current experience points. XP Needed for Next Level: the amount of XP required to increase level. Daily Production Rate: the amount added to your treasury over 24h. Cost of one XP: the treasury amount required per XP increased",
-  inventory: "The asset you own across different chains. Attack/Defend Factories allow you to mint better attack/defend assets. You can upgrade factories or individual assets. When minting an attack/defend asset, the resulting level will depend on the level of the best factory you own in the corresponding chain, and on whether you have enough treasury to mint assets of that level. All your assets can be traded on the corresponding chains.",
+  userInformation: "Homechain: the chain you support, which allows you to vote for its Chain Daily Actions. Score: your overall score, earned through different actions in the game",
+  userActivity: "All events related to you: your mints, attacks, and upgrades, other players planning to attack you, etc.",
+  homeBase: "Your Home Base generates treasury resources constantly. Upgrade: attempts to level up your home base to increase its production rate; the upgrade will not be applied unless you have enough balance in your treasury. XP: your current experience points. XP Needed for Next Level: the amount of XP required to increase the level. Daily Production Rate: the amount added to your treasury over 24 hours. Cost of one XP: the treasury amount required per XP increased",
+  inventory: "The assets you own across different chains. Attack/Defend Factories allow you to mint better attack/defend assets. You can upgrade factories or individual assets. When minting an attack/defend asset, the resulting level will depend on the level of the best factory you own in the corresponding chain, and whether you have enough treasury to mint assets of that level. All your assets can be traded on the corresponding chains",
   players: "All players in the game. Click on View Details to view their assets and initiate attacks",
   universeSummary: "Chronological log of all activities across all chains, including attacks, upgrades, and chain actions. The Player column specifies the target of a log. The Chain column specifies the Chain Daily Actions",
-  chainDailyDecision: "Vote on you homechain Daily Action, which happens at 2pm UTC. Votes are weighted by every voter's treasury and reset after execution of the action. Improve will increase XP and health of all assets in your homechain. Defend will significantly reduce potential attacks by other chain. Attack will infringe damage on an entire chain, part of it, or one single address on it",
-  chainActionProposals: "All current proposals across different chains and their total vote counts. Votes are weighted by every voter's treasury and reset after execution of the action",
+  chainDailyDecision: "Vote on your homechain Daily Action, which happens at 2 PM UTC. Votes are weighted by each voter's treasury and reset after execution of the action. 'Improve' will increase XP and health of all assets in your homechain. 'Defend' will significantly reduce potential attacks by other chains. 'Attack' will inflict damage on an entire chain, part of it, or one single address on it. Leave a comment on-chain for others to support you.",
+  chainActionProposals: "All current proposals across different chains and their total vote counts. Votes are weighted by each voter's treasury and reset after execution of the action",
   map: "Map",
 
 } 
